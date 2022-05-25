f232f1c18bb7db27d29bb0bd61bda561ee5239cb
claymc3 <mary.clay@stjude.org>
2022-05-25 09:43:13 -0500

    New script from visualizing manual restraints being used in cyana calculation before run

---
+++ preCYANA.py
@@ -0,0 +1,243 @@
+### Mary Clay
+import os
+import sys
+replacements ={
+'ALAHA':'CA','ALAQB':'CB','ALAHB1':'CB','ALAHB2':'CB','ALAHB3':'CB',
+'CYSHA':'CA','CYSHB2':'CB','CYSHB3':'CB','CYSQB':'CB',
+'ASPHA':'CA','ASPHB2':'CB','ASPHB3':'CB','ASPQB':'CB',
+'GLUHA':'CA','GLUHB2':'CB','GLUHB3':'CB','GLUQB':'CB','GLUHG2':'CG','GLUHG3':'CG','GLUQG':'CG',
+'PHEHA':'CA','PHEHB2':'CB','PHEHB3':'CB','PHEQB':'CB','PHEQD':'CD1,CD2','PHEQE':'CE1,CE2','PHEHD1':'CD1','PHEHE1':'CE1','PHEHZ':'CZ','PHEHE2':'CE2','PHEHD2':'CD2',
+'GLYHA2':'CA','GLYHA3':'CA','GLYQA':'CA',
+'HISHA':'CA','HISHB2':'CB','HISHB3':'CB','HISQB':'CB','HISHD1':'ND1','HISHE2':'NE2','HISHD2':'CD2','HISHE1':'CE1',
+'ILEHA':'CA','ILEHB':'CB','ILEQG2':'CG2','ILEHG21':'CG2','ILEHG22':'CG2','ILEHG23':'CG2','ILEHG12':'CG1','ILEHG13':'CG1','ILEQG1':'CG1','ILEQD1':'CD1','ILEHD11':'CD1','ILEHD12':'CD1','ILEHD13':'CD1',
+'LYSHA':'CA','LYSHB2':'CB','LYSHB3':'CB','LYSQB':'CB','LYSHG2':'CG','LYSHG3':'CG','LYSHD2':'CD ','LYSHD3':'CD ','LYSQD':'CD ','LYSHE2':'CE','LYSHE3':'CE','LYSQE':'CE','LYSHZ1':'NZ','LYSHZ2':'NZ','LYSHZ3':'NZ','LYSQZ':'NZ',
+'LEUHA':'CA','LEUHB2':'CB','LEUHB3':'CB','LEUQB':'CB','LEUHG':'CG','LEUHD11':'CD1','LEUHD12':'CD1','LEUHD13':'CD1','LEUQD1':'CD1','LEUHD21':'CD2','LEUHD22':'CD2','LEUHD23':'CD2','LEUQD2':'CD2','LEUQQD':'CD2,CD1',
+'METHA':'CA','METHB2':'CB','METHB3':'CB','METQB':'CB','METHG2':'CG','METHG3':'CG','METQG':'CG','METQE':'CE','METHE1':'CE','METHE2':'CE','METHE3':'CE',
+'ASNHA':'CA','ASNHB2':'CB','ASNHB3':'CB','ASNQB':'CB','ASNHD21':'ND2','ASNHD22':'ND2','ASNQD2':'ND2',
+'PROHA':'CA','PROHB2':'CB','PROHB3':'CB','PROQB':'CB','PROHG2':'CG','PROHG3':'CG','PROQG':'CG','PROHD2':'CD','PROHD3':'CD','PROQD':'CD',
+'GLNHA':'CA','GLNHB2':'CB','GLNHB3':'CB','GLNQB':'CB','GLNHG2':'CG','GLNHG3':'CG','GLNQG':'CG','GLNHE21':'NE2','GLNHE22':'NE2','GLNQE2':'NE2',
+'ARGHA':'CA','ARGHB2':'CB','ARGHB3':'CB','ARGQB':'CB','ARGHG2':'CG','ARGHG3':'CG','ARGQG':'CG','ARGHD2':'CD','ARGHD3':'CD','ARGQD':'CD','ARGHE':'NE','ARGHH11':'NH1','ARGHH12':'NH1','ARGQH1':'NH1','ARGHH21':'NH2','ARGHH22':'NH2','ARGQH2':'NH2',
+'SERHA':'CA','SERHB2':'CB','SERHB3':'CB','SERHG':'OG',
+'THRHA':'CA','THRHB':'CB','THRHG1':'OG1','THRHG21':'CG2','THRHG22':'CG2','THRHG23':'CG2','THRQG2':'CG2',
+'VALHA':'CA','VALHB':'CB','VALHG11':'CG1','VALHG12':'CG1','VALHG13':'CG1','VALQG1':'CG1','VALHG21':'CG2','VALHG22':'CG2','VALHG23':'CG2','VALQG2':'CG2','VALQQG':'CG1,CG2',
+'TRPHA':'CA','TRPHB2':'CB','TRPHB3':'CB','TRPQB':'CB','TRPHD1':'CD1','TRPHE3':'CE3','TRPHE1':'NE1','TRPHZ3':'CZ3','TRPHZ2':'CZ2','TRPHH2':'CH2',
+'TYRHA':'CA','TYRHB2':'CB','TYRHB3':'CB','TYRQB':'CB','TYRQD':'CD1,CD2','TYRQE':'CE1,CE2','TYRHD1':'CD1','TYRHE1':'CE1','TYRHE2':'CE2','TYRHD2':'CD2','TYRHH':'OH'}
+#'ALAH':'N','CYSH':'N','ASPH':'N','GLUH':'N','PHEH':'N','GLYH':'N','HISH':'N','ILEH':'N','LYSH':'N','LEUH':'N','METH':'N','ASNH':'N','GLNH':'N','ARGH':'N','SERH':'N','THRH':'N','VALH':'N','TRPH':'N','TYRH':'N',
+if len(sys.argv)==1:
+	print('''
+
+Usage: 
+	cyanapra [pdb] [upl]
+
+Required Input:
+
+	PDB			PDB to be used typically the final.pdb or pdb after CNS refinment
+				If this is not located in current directory provide path
+					CNS/refinePDB/r12_cya.pdb
+
+	upl				What upl file would you like to use? final.upl cycle?.upl
+					Which ever upl you specify determines the overveiw file used. 
+
+OutPut:
+	name_pra.cxc
+	name_pra.pml
+	Pseudobond/Distance Groups from manual restraints:
+		input.upl
+		hbond.upl 
+	Anotated Constraints files 
+''')
+	exit()
+## Dictionaries for assigning color to pml and cxc scripts for the peak list restraints
+colors = ['white','raspberry','gold','forest','marine','purple','orange','cyan','pink','deepteal','gray']
+colors2 = ['white','mediumvioletred','orange','forest','royalblue','purple','chocolate','cyan','pink','deepteal','gray']
+
+cwd = os.getcwd() + '/'
+outdir = cwd + 'pre_cyana/'
+in_pdb = sys.argv[1]
+fupl = sys.argv[2]
+pdbname = in_pdb.split('.')[0]
+calc = cwd + 'CALC.cya'
+outname = fupl.split('.')[0]
+
+## Check for the output directory if it does not exist make it
+if not os.path.exists(outdir):
+	os.makedirs(outdir)
+
+checkcons = open(outdir + outname + '_summary.txt','w')
+checkcons.write('                         #peaks    upl Violations Assigned Ambiguous Unassigned\n')
+
+## open the CALC.cya file to get the peaks list and additonal constraint files used in the calculation. 
+manualongcons = [line.strip() for line in open(calc).readlines() if line.strip() and '.upl' in line][0].split()[2].split(',')
+print(manualongcons)
+upls = [con for con in manualongcons if 'upl' in con and 'hbond' not in con]
+lols = [con for con in manualongcons if 'lol' in con and 'hbond' not in con]
+dihed = [con for con in manualongcons if 'aco' in con]
+
+
+outpml = open(outdir + 'CYANA_input.pml','w')
+outpml.write('load '+ cwd + in_pdb+'\n')
+outpml.write('set dash_gap, 0.05\n')
+outpml.write('color gray60, all\n')
+outcmx = open(outdir + 'CYANA_input.cxc','w')
+outcmx.write('open '+ cwd + in_pdb+'\n')
+outcmx.write('color #1 gray(150)\n')
+pdbname = in_pdb.replace('.pdb','')
+
+mn = 2
+print(mns)
+cmxphisel, cmxchisel = 'name phipsisel #%s:'%mn, 'name chisel #%s:'%(mn+=1)
+pmlphisel, pmlchisel = 'create phi-psi, %s_0001 and resi '%pdbname,'create chi, %s_0001 and resi ' %pdbname
+mcount = 0
+for line in open(in_pdb).readlines():
+	if 'MODEL ' in line:mcount+=1
+
+print(mcount)
+if mcount > 2:cmxn = '#1.1'
+if mcount <= 1:cmxn = '#1'
+
+u = 0
+for uplfile in upls:
+	fin = open(uplfile,'r')
+	outpb = open(outdir + uplfile.replace('.upl','_cons.pb'),'w')
+	pmlgroup = 'group %s, ' %(uplfile.replace('.upl',''))
+	outpb.write("; halfbond = false\n; color = blue\n; radius = 0.1\n; dashes = 10\n")
+	for line in fin.readlines():
+		cns = line.split()
+		if "#" not in cns[0]:
+			u+=1
+			atom1 = cns[2]
+			atom2 = cns[5]
+			if cns[1]+cns[2] in replacements.keys():
+				atom1 = atom1.replace(cns[2], replacements[cns[1]+cns[2]])
+			if cns[4]+cns[5] in replacements.keys():
+				atom2 = atom2.replace(cns[5], replacements[cns[4]+cns[5]])
+			if cns[1]+cns[5] not in replacements.keys():
+				atom1 = atom1
+			if cns[4]+cns[5] not in replacements.keys():
+				atom2=atom2
+			if ',' in atom1 and ',' not in atom2:
+				outpb.write('%s:%s@%s %s:%s@%s\n' %(cmxn, cns[0], atom1.split(',')[0], cmxn,  cns[3],atom2))
+				outpb.write('%s:%s@%s %s:%s@%s\n' %(cmxn, cns[0], atom1.split(',')[1], cmxn, cns[3],atom2))
+				outpml.write('distance %s%s, %s and resi %s and name %s, %s and resi %s and name %s\n' %(uplfile.replace('.upl',''),str(u), pdbname, cns[0], atom1.split(',')[0], pdbname, cns[3], atom2))
+				pmlgroup = pmlgroup + uplfile.replace('.upl','') + str(u) + ' '
+				u+=1
+				outpml.write('distance %s%s, %s and resi %s and name %s, %s and resi %s and name %s\n' %(uplfile.replace('.upl',''),str(u), pdbname, cns[0], atom1.split(',')[1], pdbname, cns[3], atom2))
+				pmlgroup = pmlgroup + uplfile.replace('.upl','') + str(u) + ' '
+			if ',' in atom2 and ',' not in atom1:
+				outpb.write('%s:%s@%s %s:%s@%s\n' %(cmxn, cns[0], atom1, cmxn, cns[3],atom2.split(',')[0]))
+				outpb.write('%s:%s@%s %s:%s@%s\n' %(cmxn, cns[0], atom1, cmxn, cns[3],atom2.split(',')[1]))
+				outpml.write('distance %s%s, %s and resi %s and name %s, %s and resi %s and name %s\n' %(uplfile.replace('.upl',''),str(u), pdbname, cns[0], atom1, pdbname, cns[3], atom2.split(',')[0]))
+				pmlgroup = pmlgroup + uplfile.replace('.upl','') + str(u) + ' '
+				u+=1
+				outpml.write('distance %s%s, %s and resi %s and name %s, %s and resi %s and name %s\n' %(uplfile.replace('.upl',''),str(u), pdbname, cns[0], atom1, pdbname, cns[3], atom2.split(',')[2]))
+				outpml.write('color pink, %s%s\n'%(uplfile.replace('.upl',''),str(u)))
+				pmlgroup = pmlgroup + uplfile.replace('.upl','') + str(u) + ' '
+			if ',' not in atom1 and ',' not in atom2:
+				outpb.write('%s:%s@%s %s:%s@%s\n' %(cmxn, cns[0], atom1, cmxn, cns[3],atom2))
+				outpml.write('distance %s%s, %s and resi %s and name %s, %s and resi %s and name %s\n' %(uplfile.replace('.upl',''),str(u), pdbname, cns[0], atom1, pdbname, cns[3], atom2))
+				pmlgroup = pmlgroup + uplfile.replace('.upl','') + str(u) + ' '
+	outpml.write(pmlgroup + '\n')
+	outpml.write('color blue,' + uplfile.replace('.upl','') + '\n')
+	mn+=1
+	outcmx.write('open ' + outdir + uplfile.replace('.upl','_cons.pb') + '\n')
+	outcmx.write('color #%s %s\n' %(str(mn),'cyan'))
+
+selhbond = 'name hbond  #1.1:'
+hbonsl = []
+hbond = open(outdir + 'hbond_cons.pb','w')
+hbond.write("; halfbond = false\n; color = pink\n; radius = 0.2\n; dashes = 10\n")
+hbgroupline = 'group hbond , '
+h = 1
+mn+=1
+for line in open('hbond.upl').readlines():
+	cns = line.split()
+	if "#" not in cns[0]:
+		if (cns[0],cns[3]) not in hbonsl:
+			h+=1 
+			hbonsl.append((cns[0],cns[3]))
+			hbonsl.append((cns[3],cns[0]))
+			hbond.write('%s:%s@%s #%s:%s@%s %s\n' %(cmxn, cns[0], cns[2], cmxn, cns[3],cns[5],'pink'))
+			outpml.write('distance hbond%s, %s and resi %s and name %s, %s and resi %s and name %s\n' %(str(h), pdbname, cns[0], cns[2].replace('H','N'), pdbname, cns[3], cns[5].replace('H','N')))
+			hbgroupline = hbgroupline + 'hbond' + str(h) + ' '
+			if cns[0] not in selhbond:
+				selhbond = selhbond +'%s,' %(cns[0])
+			if cns[3] not in selhbond:
+				selhbond = selhbond +'%s,' %(cns[3])
+hbond.close()
+outpml.write(hbgroupline + '\n')
+outpml.write('color pink, hbond\n')
+selhbond = selhbond[:-1] + '@O,N\n'
+outcmx.write('open ' + outdir + 'hbond_cons.pb\n')
+outcmx.write('color #%s %s\n' %(str(mn),'pink'))
+outcmx.write(selhbond)
+
+phir, chir = [],[]
+for angf in dihed:
+	for line in open(angf).readlines():
+		if '#' not in line and line.strip():
+			ang = line.split()
+			if ang[2] == 'PHI' or ang[2] == 'PSI':
+				if ang[0] not in phir:
+					phir.append(ang[0])
+					cmxphisel = cmxphisel + ang[0] + ','
+					pmlphisel = pmlphisel + ang[0] + '+'
+			if 'CHI' in ang[2]:
+				if ang[0] not in chir:
+					chir.append(ang[0])
+					cmxchisel = cmxchisel + ang[0] + ','
+					pmlchisel = pmlchisel  + ang[0] + '+'
+
+
+# outcmx.write('combine #1.1 modelId %s name angles\n' %mns)
+outcmx.write('combine %s modelId %s name phi-psi\n'%(cmxn, mn+1))
+outcmx.write(cmxphisel[:-1] + '\n')
+outcmx.write('color phipsisel purple target ac\n')
+outcmx.write('combine %s modelId %s name chi\n'%(cmxn, mn+2))
+outcmx.write(cmxchisel[:-1] + '\n')
+outcmx.write('color chisel cornflower blue target ac \n')
+outcmx.write('combine %s modelId %s name viol_phi-psi\n'%(cmxn, mn+3))
+outcmx.write(cmxphiviol[:-1] + '\n')
+outcmx.write('color phipsiviol hot pink target ac \n')
+outcmx.write('combine %s modelId %s name viol_chi\n'%(cmxn,mn+4))
+outcmx.write(cmxchiviol[:-1] + '\n')
+outcmx.write('color chiviol mediumvioletred target ac\n')
+
+outpml.write(pmlphisel[:-1] + '\n')
+outpml.write('color purple,phi-psi\n')
+outpml.write(pmlchisel[:-1] + '\n')
+outpml.write('color marin, chi\n')
+outpml.write(pmlchiviol[:-1] + '\n')
+outpml.write('color magenta, viol_phi-psi \n')
+outpml.write(pmlphiviol[:-1] + '\n')
+outpml.write('color red, viol_chi\n')
+outpml.write("hide labels\n")
+outpml.write('color gray60, final\n')
+outpml.write('split_states ' + pdbname + '\n')
+
+if mcount > 2:
+	for y in range(2,21,1):
+		outcmx.write('match #1.%s to #1.1\n' %str(y))
+		outpml.write('align %s_%04d, %s_0001\n' %(pdbname,y, pdbname))
+
+outcmx.write('color :thr teal target a\n')
+outcmx.write('color :thr teal target a\n')
+outcmx.write('color :val orange  target a\n')
+outcmx.write('color :leu indian red  target a\n')
+outcmx.write('color :met purple  target a\n')
+outcmx.write('color :ala forest green  target a\n')
+outcmx.write('color :ile dodger blue  target a\n')
+outcmx.write('color :phe slate blue target a\n')
+outcmx.write('color :tyr orchid target a\n')
+outcmx.write('color  byhetero target a\n')
+outcmx.write('show :thr,met,ala,leu,val,ile,phe,tyr\n')
+outcmx.write('hide H\n')
+outcmx.write('show #1.1 @H\n')
+outcmx.write('show #1.1@N target a\n')
+outcmx.write('cartoon suppress false\n')
+outcmx.write('label #1.1  text "{0.label_one_letter_code}{0.number}{0.insertion_code}"\n')
+outcmx.write('label ontop false\n')
+outcmx.write('ui tool show "Side View"')
+outpml.close()
+outcmx.close()
+
+
