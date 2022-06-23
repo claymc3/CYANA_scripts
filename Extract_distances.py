e5d93230fc8bc693cf025956b93fccbe7ee41ce9
claymc3 <90853597+claymc3@users.noreply.github.com>
2022-06-23 09:57:18 -0500

    Script can now create all possible pseudo atoms but handels only the final upl

--- Extract_distances.py
+++ Extract_distances.py
@@ -47,15 +47,15 @@ d (the distance), and dminus, and dplus
 CYANA upl/lol format 
 632 LYS  H     633 ILE  H       4.46  #peak 1 #plist 1 #SUP 0.91 #QF 0.91
 '''
-DistancesDF = pd.DataFrame()
+DistancesDF = pd.DataFrame(columns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,'mean','stdv','upl','plist','peak'])
 
 
-in_pdb = '/Users/mclay1/FGFR3_structure/cyana_23/final.pdb'
-in_upl = '/Users/mclay1/FGFR3_structure/cyana_23/final.upl'
-chains = 'A'
-Coords = {}
-Sequence = []
-# PDB_df = pd.DataFrame(columns=pdb_columns)
+cwd = '/Volumes/common/Kinases/FGFR3/FGFR3_459-755_C482A_C582S/Structure_Calc/cyana_23/'
+# cwd = '/Users/mclay1/FGFR3_structure/cyana_23/'
+in_pdb = cwd + 'final.pdb'
+in_upl = cwd + 'final.upl'
+
+
 Starts = []
 Ends = []
 for mnum in range(1,21,1):
@@ -71,57 +71,67 @@ for (start,end) in zip(Starts,Ends):
 	n+=1
 	Coor = eval('Coor' + str(n))
 	for x in range(start,end,1):
+		print('Reading coordinates for model %d' %n)
 		line = pdb[x]
 		if line[0:4] == "ATOM" or line[0:4] == 'HETA':
-			index =  line[17:20].strip() + '-' + line[22:26].strip() + '-' + line[12:16].strip()
+			index = '%3s %4s %-4s'%(line[17:20].strip(),line[22:26].strip(),line[12:16].strip())
 			Coor[index] = [float(line[30:38]),float(line[38:46]),float(line[46:54])]
+			## Generate pseudo atoms for methyl groups
+			# print(index)
 			if line[17:20].strip() in ['ILE','LEU','ALA','VAL','MET','THR'] and line[12:16].strip() in ['HB1','HE1','HD11','HD21','HG11','HG21']:
-				index =  line[17:20].strip() + '-' + line[22:26].strip() + '-' + line[12:16].strip()[:-1].replace('H','Q')
-				print(line[12:16].strip())
+				index = '%3s %4s %-4s'%(line[17:20].strip(),line[22:26].strip(),line[12:16].strip()[:-1].replace('H','Q'))
+				# print('methyl ' + index)
 				line2 = pdb[x+1]
 				line3 = pdb[x+2]
-				print(line2[12:16].strip())
-				print(line3[12:16].strip())
 				xcoor = (float(line[30:38]) + float(line2[30:38]) + float(line3[30:38]))/3.0
 				ycoor = (float(line[38:46]) + float(line2[38:46]) + float(line3[38:46]))/3.0
 				zcoor = (float(line[46:54]) + float(line2[46:54]) + float(line3[46:54]))/3.0
 				Coor[index] = [xcoor,ycoor,zcoor]
-			if line[17:20].strip() not in ['ILE','LEU','ALA','VAL','MET','THR'] and len(line[12:16].strip()) == 4:
-				index =  line[17:20].strip() + '-' + line[22:26].strip() + '-' + 'Q' + line[13:16].strip()[:-1]
-				line2 = pdb[x+1]
-				xcoor = (float(line[30:38]) + float(line2[30:38]))/2.0
-				ycoor = (float(line[38:46]) + float(line2[38:46]))/2.0
-				zcoor = (float(line[46:54]) + float(line2[46:54]))/2.0
-				Coor[index] = [xcoor,ycoor,zcoor]
-			if line[17:20].strip() in ['PHE','TYR'] and line[12:16].strip() == 'HB2':
-				index =  line[17:20].strip() + '-' + line[22:26].strip() + '-' + 'QB'
-				line2 = pdb[x+1]
-				xcoor = (float(line[30:38]) + float(line2[30:38]))/2.0
-				ycoor = (float(line[38:46]) + float(line2[38:46]))/2.0
-				zcoor = (float(line[46:54]) + float(line2[46:54]))/2.0
-				Coor[index] = [xcoor,ycoor,zcoor]
+			## Generate pseudo atoms for side chain exchangeable atoms 
+			if line[17:20].strip() not in ['ILE','LEU','ALA','VAL','MET','THR'] and line[12:16].strip()[0] == 'H':
+				if len(line[12:16].strip()) == 4 and line[12:16].strip()[-1] == '1':
+					index = '%3s %4s %-4s'%(line[17:20].strip(),line[22:26].strip(),'Q' + line[12:16].strip()[1:-1])
+					# print('side chain '+ index)
+					line2 = pdb[x+1]
+					xcoor = (float(line[30:38]) + float(line2[30:38]))/2.0
+					ycoor = (float(line[38:46]) + float(line2[38:46]))/2.0
+					zcoor = (float(line[46:54]) + float(line2[46:54]))/2.0
+					Coor[index] = [xcoor,ycoor,zcoor]
+				## Generate pseudo atoms for aromatic atoms 
+				if len(line[12:16].strip()) == 3 and '2' in line[12:16].strip():
+					index =  '%3s %4s %-4s'%(line[17:20].strip(),line[22:26].strip(),'Q' + line[12:16].strip()[1:-1])
+					# print('side chain2 ' + index)
+					line2 = pdb[x+1]
+					xcoor = (float(line[30:38]) + float(line2[30:38]))/2.0
+					ycoor = (float(line[38:46]) + float(line2[38:46]))/2.0
+					zcoor = (float(line[46:54]) + float(line2[46:54]))/2.0
+					Coor[index] = [xcoor,ycoor,zcoor]
+
 for line in open(in_upl).readlines():
 	if 'SUP' not in line:
 		pass 
 	else:
 		cns = line.strip().split()
-		atom1 = cns[1].replace('HIST','HIS') + '-' + cns[0] + '-' + cns[2]
-		atom2 = cns[4].replace('HIST','HIS') + '-' + cns[3] + '-' + cns[5]
+		atom1 = '%3s %4s %-4s'%(cns[1].replace('HIST','HIS'), cns[0],cns[2])
+		atom2 = '%3s %4s %-4s'%(cns[4].replace('HIST','HIS'),cns[3],cns[5])
 		for mnum in range(1,21,1):
 			Coor = eval('Coor' + str(mnum))
 			(x1,y1,z1) = Coor[atom1]
 			(x2,y2,z2) = Coor[atom2]
 			d = np.round(np.sqrt((x1-x2)**2 + (y1-y2)**2+ (z1-z2)**2),2)
-			DistancesDF.loc[atom1 + '-' + atom2,mnum] = d
+			DistancesDF.loc[atom1 + ' ' + atom2,mnum] = d
 	DistancesDF['mean'] = np.round(DistancesDF.mean(axis=1),2)
+	DistancesDF['stdv'] = np.round(DistancesDF.std(axis=1),2)
 for line in open(in_upl).readlines():
 	if 'SUP' not in line:
 		pass 
 	else:
 		cns = line.strip().split()
-		atom1 = cns[1].replace('HIST','HIS') + '-' + cns[0] + '-' + cns[2]
-		atom2 = cns[4].replace('HIST','HIS') + '-' + cns[3] + '-' + cns[5]
-	DistancesDF.loc[atom1 + '-' + atom2,'upl'] = cns[6]
+		atom1 = '%3s %4s %-4s'%(cns[1].replace('HIST','HIS'), cns[0],cns[2])
+		atom2 = '%3s %4s %-4s'%(cns[4].replace('HIST','HIS'),cns[3],cns[5])
+	DistancesDF.loc[atom1 + ' ' + atom2,'upl'] = cns[6]
+	DistancesDF.loc[atom1 + ' ' + atom2,'peak'] = cns[8]
+	DistancesDF.loc[atom1 + ' ' + atom2,'plist'] = cns[10]
 DistancesDF.to_csv('test.csf')
 print(DistancesDF)
 print(DistancesDF.shape)
