5cbd43388b88d592dcbbf909f5a4fc8b2e3947fd
claymc3 <90853597+claymc3@users.noreply.github.com>
2022-10-12 10:31:02 -0500

    Starting the analysis of unused peaks

--- NOESY_list.py
+++ NOESY_list.py
@@ -9,13 +9,55 @@
 # ------------------------------------------------------------------------------
 
 '''
+import pandas as pd
 import os
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.backends.backend_pdf import PdfPages
+from scipy.stats import trim_mean
+from scipy.stats import tstd
+from matplotlib.ticker import FormatStrFormatter
+import matplotlib.ticker as ticker 
+import seaborn as sb
+import re
+####----------------------------------------------------------------------------------------------####
+##																									##
+##			 	Setting controlling plot appearance: Font, line widths, ticks, and legend  			##
+##																									##
+####----------------------------------------------------------------------------------------------####
 
+linewidths = 1.5
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['font.sans-serif'] = 'arial'
+mpl.rcParams['font.size'] = 10
+mpl.rcParams['axes.linewidth'] = linewidths
+mpl.rcParams['xtick.direction'] = mpl.rcParams['ytick.direction']='out'
+mpl.rcParams['xtick.labelsize'] = mpl.rcParams['ytick.labelsize']=10
+mpl.rcParams['xtick.major.size'] = mpl.rcParams['ytick.major.size'] = 5
+mpl.rcParams['xtick.major.width'] = mpl.rcParams['ytick.major.width']=linewidths
+mpl.rcParams['xtick.minor.size'] = 2
+mpl.rcParams['xtick.minor.width'] = linewidths
+mpl.rcParams['axes.spines.right'] = False
+mpl.rcParams['axes.spines.top'] = False
+mpl.rcParams['legend.fontsize'] = 10
+mpl.rcParams['legend.loc'] = 'best'
+mpl.rcParams['legend.borderpad'] = 0.01
+mpl.rcParams['legend.frameon'] = False
+mpl.rcParams['legend.handlelength'] = 0
+mpl.rcParams['legend.scatterpoints'] = 1
+mpl.rcParams['xtick.major.bottom'] = mpl.rcParams['ytick.major.left'] = True
+mpl.rcParams['xtick.major.top'] = mpl.rcParams['ytick.major.right'] = True
+mpl.rcParams['mathtext.fontset'] = 'cm'
+mpl.rcParams['mathtext.sf'] = 'sans\\-serif'
+plt.rcParams['mathtext.default'] = 'regular'
+mpl.rcParams['xtick.major.pad']=mpl.rcParams['ytick.major.pad']= 2
+mpl.rcParams['axes.labelpad'] = 3
+mpl.mathtext.FontConstantsBase.sup1 = 0.25
 
 # cwd = '/Volumes/common/Kinases/FGFR3/FGFR3_459-755_C482A_C582S/Structure_Calc/cyana_25/'
-cwd = '/Users/mclay1/FGFR3_structure/cyana_23/'
+cwd = '/Volumes/common/Kinases/FGFR2/FGFR2_467-768_C491A/Structure_Calc/cyana_21/'
 calc = cwd + 'CALC.cya'
 cya_plists = [line.strip() for line in open(calc).readlines() if line.strip() and 'peaks' in line][0].split()[2].split(',')
 noa = cwd + 'cycle7.noa'
@@ -24,6 +66,7 @@ print(cya_plists)
 
 noalines = open(noa).readlines()
 pl = 0
+unusedupls = []
 unusedupl = open('final_unused_3.upl','w')
 usedupl = open('final_used.upl','w')
 Used_upls = []
@@ -75,10 +118,12 @@ for noelist in cya_plists:
 						atom1,resn1, resi1, atom2, resn2, resi2,pshift, maxd = cns[0],cns[1],int(cns[2]), cns[4],cns[5],int(cns[6]), float(cns[9])/100, float(cns[12].split('-')[0])
 					upl = '%4d %-4s %-4s %4d %-4s %-4s' %(resi1, resn1, atom1, resi2, resn2, atom2)
 					upl2 = '%4d %-4s %-4s %4d %-4s %-4s' %(resi2, resn2, atom2, resi1, resn1, atom1)
-					if upl not in Used_upls and pshift > 0.9 and maxd < 8.0:
+					if upl not in Used_upls and pshift > 0.9 and maxd < 9.0:
 						# print(upl)
 						Used_upls.extend([upl,upl2])
-						unusedupl.write('%4d %-4s %-4s %4d %-4s %-4s    %s  #peak %s #plist %d #Pshift %0.2f\n' %(resi1, resn1, atom1, resi2, resn2, atom2, dist, peakn, pl, pshift ))
+						unusedupl.write('{:<4d} {:^4s} {:^4s} {:<4d} {:^4s} {:^4s}    {:}  #peak {:} #plist {:} #Pshift {:0.2f} out of {:}\n'.format(resi1, resn1, atom1, resi2, resn2, atom2, dist, peakn, pl, pshift,noalines[x+1].split()[3]))
+						unusedupls.append('{:<4d} {:^4s} {:^4s} {:<4d} {:^4s} {:^4s}   #plist {:} #options {:}'.format(resi1, resn1, atom1, resi2, resn2, atom2, pl,noalines[x+1].split()[3]))
+
 	print(noelist)
 	print("Total number of peaks %d" %peak)
 	print('Number with single assignment %d'%single)
