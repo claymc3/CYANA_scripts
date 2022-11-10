6d01a340b0a6f80e67b64914407647f7b7006fd4
claymc3 <90853597+claymc3@users.noreply.github.com>
2022-11-10 16:56:50 -0600

    formatting changes

--- noa_analysis.py
+++ noa_analysis.py
@@ -31,33 +31,6 @@ import mplcursors
 ####----------------------------------------------------------------------------------------------####
 AAA_dict = {"ALA": "A", "ARG": "R", "ASN": "N", "ASP": "D", "CYS": "C", "GLU": "E", "GLN": "Q", "GLY": "G", "HIS": "H","HIST": "H", "ILE": "I", "LEU": "L", "LYS": "K", "MET": "M", "PHE": "F", "PRO": "P", "SER": "S", "THR": "T", "TRP": "W", "TYR": "Y", "VAL": 'V', "MSE":'M', "PTR":'Y', "TPO":"T", "SEP":'S'}
 
-linewidths = 1.5
-mpl.rcParams['pdf.fonttype'] = 42
-mpl.rcParams['font.sans-serif'] = 'arial'
-mpl.rcParams['font.size'] = 10
-mpl.rcParams['axes.linewidth'] = linewidths
-mpl.rcParams['xtick.direction'] = mpl.rcParams['ytick.direction']='out'
-mpl.rcParams['xtick.labelsize'] = mpl.rcParams['ytick.labelsize']=10
-mpl.rcParams['xtick.major.size'] = mpl.rcParams['ytick.major.size'] = 5
-mpl.rcParams['xtick.major.width'] = mpl.rcParams['ytick.major.width']=linewidths
-mpl.rcParams['xtick.minor.size'] = 2
-mpl.rcParams['xtick.minor.width'] = linewidths
-# mpl.rcParams['axes.spines.right'] = False
-# mpl.rcParams['axes.spines.top'] = False
-mpl.rcParams['legend.fontsize'] = 10
-mpl.rcParams['legend.loc'] = 'best'
-mpl.rcParams['legend.borderpad'] = 0.01
-mpl.rcParams['legend.frameon'] = False
-mpl.rcParams['legend.handlelength'] = 0
-mpl.rcParams['legend.scatterpoints'] = 1
-mpl.rcParams['xtick.major.bottom'] = mpl.rcParams['ytick.major.left'] = True
-mpl.rcParams['xtick.major.top'] = mpl.rcParams['ytick.major.right'] = True
-mpl.rcParams['mathtext.fontset'] = 'cm'
-mpl.rcParams['mathtext.sf'] = 'sans\\-serif'
-plt.rcParams['mathtext.default'] = 'regular'
-mpl.rcParams['xtick.major.pad']=mpl.rcParams['ytick.major.pad']= 2
-mpl.rcParams['axes.labelpad'] = 3
-# mpl.mathtext.FontConstantsBase.sup1 = 0.25
 
 # cwd = '/Volumes/common/Kinases/FGFR3/FGFR3_459-755_C482A_C582S/Structure_Calc/cyana_25/'
 cwd = '/Volumes/common/Kinases/FGFR2/FGFR2_467-768_C491A/Structure_Calc/cyana_46/'
@@ -98,7 +71,7 @@ for x in range(len(noalines)):
 	line = noalines[x]
 	if 'Peak' in noalines[x] and 'out of' in noalines[x+1]:
 		if '0 out of' not in noalines[x+1] and 'diagonal' not in noalines[x]:
-			peak = noalines[x].strip().split()[1]
+			peak = int(noalines[x].strip().split()[1])
 			plist = noalines[x].strip().split()[3]
 			for y in range(2,int(noalines[x+1].split()[0])+2,1):
 				cns = noalines[x+y].strip().split()
@@ -108,7 +81,7 @@ for x in range(len(noalines)):
 					atom1,resn1, resi1, atom2, resn2, resi2,pshift,drange = cns[0],cns[1],int(cns[2]), cns[4],cns[5],int(cns[6]), float(cns[9])/100, cns[12]
 				group1 = '{:}{:}-{:}'.format(AAA_dict[resn1],resi1, atom1)
 				group2 = '{:}{:}-{:}'.format(AAA_dict[resn2],resi2, atom2)
-				outline = '    {:<10} - {:<10}   {:<9}  #peak {:} from {:}  pshift {:0.2f}\n'.format(group1, group2,drange,peak,plist,pshift)
+				outline = '    {:>10} - {:<10}   {:<9}  Peak {:} from {:}  pshift {:0.2f}\n'.format(group1, group2,drange,peak,plist,pshift)
 				if '{:}-{:}'.format(group1,group2)in ADpairs:
 					assigndict['{:}-{:}'.format(group1,group2)].append(outline)
 				if '{:}-{:}'.format(group2,group1)in ADpairs:
@@ -122,7 +95,7 @@ for x in range(len(noalines)):
 	line = noalines[x]
 	if 'Peak' in noalines[x] and 'out of' in noalines[x+1]:
 		if '0 out of' in noalines[x+1] and '0 out of 0' not in noalines[x+1]:
-			peak = noalines[x].strip().split()[1]
+			peak = int(noalines[x].strip().split()[1])
 			plist = noalines[x].strip().split()[3]
 			unused_assignment.write(noalines[x])
 			for y in range(2,int(noalines[x+1].split()[3])+2,1):
@@ -133,7 +106,7 @@ for x in range(len(noalines)):
 					atom1,resn1, resi1, atom2, resn2, resi2,pshift, drange = cns[0],cns[1],int(cns[2]), cns[4],cns[5],int(cns[6]), float(cns[9])/100, cns[12]
 				group1 = '{:}{:}-{:}'.format(AAA_dict[resn1],resi1, atom1)
 				group2 = '{:}{:}-{:}'.format(AAA_dict[resn2],resi2, atom2)
-				outline = '#    {:<10} - {:<10}   {:<9}  #peak {:} from {:}  pshift {:0.2f} unused\n'.format(group1, group2,drange,peak,plist,pshift)
+				outline = '#    {:>10} - {:<10}   {:<9}  Peak {:} from {:}  pshift {:0.2f} unused\n'.format(group1, group2,drange,peak,plist,pshift)
 				if '{:}-{:}'.format(group1,group2)in ADpairs2:
 					assigndict['{:}-{:}'.format(group1,group2)].append(outline)
 				if '{:}-{:}'.format(group2,group1)in ADpairs2:
@@ -151,15 +124,16 @@ noassignment.close()
 unused_assignment.close()
 assigned = open('assinged.list','w')
 questionable = open('Q_assinged.list','w')
+qpeaks = []
 for con in ADpairs:
 	if len(assigndict[con]) > 1:
 		assigned.write('{:}  {:}:\n'.format(con,len(assigndict[con])))
 		assigned.writelines(assigndict[con])
 		assigned.write('\n')
 	if len(assigndict[con]) == 1:
-		questionable.write('{:}  {:}:\n'.format(con,len(assigndict[con])))
-		questionable.writelines(assigndict[con])
-		questionable.write('\n')
+		qpeaks.append(assigndict[con][0])
+qpeaks = sorted(qpeaks,key = lambda x: (x.split()[7],x.split()[5]))
 assigned.close()
+questionable.writelines(qpeaks)
 questionable.close()
 
