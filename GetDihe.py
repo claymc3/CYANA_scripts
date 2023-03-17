4a5a5dd735b987c876bec5e77a74dc7466250992
claymc3 <90853597+claymc3@users.noreply.github.com>
2023-03-17 17:55:27 -0500

    now poloing on molprobity style plot for phi psi

--- GetDihe.py
+++ GetDihe.py
@@ -6,20 +6,65 @@ import sys
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib import colors
+from math import ceil, floor
+from rama_config import RAMA_PREFERENCES
+from rama_config import ROTA_PREFERENCES
+
 pdb_columns = ['name', 'resn', 'resid', 'Chain', 'X', 'Y', 'Z', 'Element']
 # Read in PDB file one line at a time, if the first four letter ar ATOM or HETA then it will parse the data into the 
 # data frame, using the atome index int he PDB as the row index in the data frame. 
 AAA_dict = {"ALA": "A", "ARG": "R", "ASN": "N", "ASP": "D", "CYS": "C", "GLU": "E", "GLN": "Q", "GLY": "G", "HIS": "H", "ILE": "I", "LEU": "L", "LYS": "K", "MET": "M", "PHE": "F", "PRO": "P", "SER": "S", "THR": "T", "TRP": "W", "TYR": "Y", "VAL": 'V' }
-A_dict = { "I": "ILE", "A": "ALA", "V": "VAL", "L": "LEU", "M": "MET", "T" : "THR","F" : "PHE", "Y": "TYR" }
+A_dict = {'C': 'CYS', 'D': 'ASP', 'S': 'SER', 'Q': 'GLN', 'K': 'LYS', 'I': 'ILE', 'P': 'PRO', 'T': 'THR', 'F': 'PHE', 'N': 'ASN','G': 'GLY', 'H': 'HIS', 'L': 'LEU', 'R': 'ARG', 'W': 'TRP','A': 'ALA', 'V':'VAL', 'E': 'GLU', 'Y': 'TYR', 'M': 'MET'}
 Methyl_groups = {"LEU" : ['CD1','CD2'], "ILE" : ['CD1'], "VAL" : ['CG1','CG2'], "THR" : ['CG2'], "MET" : ['CE'], "ALA" : ['CB']}
 Methyls = ['ILE', 'LEU','VAL','MET','ALA', 'THR']
 Aromatics = ['PHE', 'TYR']
 Aromatic_groups = {"PHE" : ['CE1', 'CE2'], "TYR": ['CE1','CHE2']}
 Pass_Atoms = ['N   ALA', 'H   ALA', 'N   ARG', 'H   ARG', 'N   ASN', 'H   ASN', 'N   ASP', 'H   ASP', 'N   CYS', 'H   CYS', 'N   GLU', 'H   GLU', 'N   GLN', 'H   GLN', 'N   GLY', 'H   GLY', 'N   HIS', 'H   HIS', 'N   ILE', 'H   ILE', 'N   LEU', 'H   LEU', 'N   LYS', 'H   LYS', 'N   MET', 'H   MET', 'N   PHE', 'H   PHE', 'N   SER', 'H   SER', 'N   THR', 'H   THR', 'N   TRP', 'H   TRP', 'N   TYR', 'H   TYR', 'N   VAL', 'H   VAL']
 
+
 if len(sys.argv) == 1:
 	print('Usage: GetDieh pdb chain')
 	exit()
+
+RAMA_PREF_VALUES = None
+ROTA_PREF_VALUES = None
+
+def _cache_RAMA_PREF_VALUES():
+	## Values in data span -179 to 179 in incrments of 2, so values are translated to 0 to 360 in increments of 1
+	f_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+	RAMA_PREF_VALUES = {}
+	for key, val in RAMA_PREFERENCES.items():
+		RAMA_PREF_VALUES[key] = np.full((360, 360), 0, dtype=np.float64)
+		with open(os.path.join(f_path, val["file"])) as fn:
+			for line in fn:
+				if line.startswith("#"):
+					continue
+				else:
+					x = int(float(line.split()[1]))
+					y = int(float(line.split()[0]))
+					RAMA_PREF_VALUES[key][x + 180][y + 180] \
+						= RAMA_PREF_VALUES[key][x + 179][y + 179] \
+						= RAMA_PREF_VALUES[key][x + 179][y + 180] \
+						= RAMA_PREF_VALUES[key][x + 180][y + 179] \
+						= float(line.split()[2]) 
+	return RAMA_PREF_VALUES
+
+def _cache_ROTA_PREF_VALUES():
+	f_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+	ROTA_PREF_VALUES = {}
+	for key, val in ROTA_PREFERENCES.items():
+		## Values in data span 5 to 255 in incrments of 2
+		ROTA_PREF_VALUES[key] = np.full((361, 361), 0, dtype=np.float64)
+		with open(os.path.join(f_path, val["file"])) as fn:
+			for line in fn:
+				if line.startswith("#"):
+					continue
+				else:
+					x = int(float(line.split()[1]))
+					y = int(float(line.split()[0]))
+					ROTA_PREF_VALUES[key][x][y] = float(line.split()[-1]) 
+	return ROTA_PREF_VALUES
 
 def crossProduct(u,v):
 	## Calculates the cross product of two 3d vectors (as 1-d arrays).
@@ -92,6 +137,89 @@ SideDihe = {
  'W':[['chi1', 'N', 'CA', 'CB','CG'], ['chi2', 'CA', 'CB', 'CG', 'CD1']]}
 
 
+def plot_phi_psi_ramachandran(pdf, PhiDF, PsiDF):
+	print('Plotting Phi-Psi')
+
+	global RAMA_PREF_VALUES
+
+	if RAMA_PREF_VALUES is None:
+		RAMA_PREF_VALUES = _cache_RAMA_PREF_VALUES()
+
+	for res in PhiDF.index.to_list():
+		normals = {"phi":[],"psi":[]}
+		outliers = {"phi":[],"psi":[]}
+		aa_type = PhiDF.loc[res,'type']
+		for mnum in range(1,21,1):
+			if RAMA_PREF_VALUES[aa_type][int(PsiDF.loc[res,mnum])+ 180][int(PhiDF.loc[res,mnum]) + 180] < RAMA_PREFERENCES[aa_type]["bounds"][1]:
+				outliers["phi"].append(PhiDF.loc[res,mnum])
+				outliers["psi"].append(PsiDF.loc[res,mnum])
+			else:
+				normals["phi"].append(PhiDF.loc[res,mnum])
+				normals["psi"].append(PsiDF.loc[res,mnum])
+
+		fig, ax =plt.subplots(figsize=(3,3))
+		plt.imshow(RAMA_PREF_VALUES[aa_type], cmap=RAMA_PREFERENCES[aa_type]["cmap"],
+				norm=colors.BoundaryNorm(RAMA_PREFERENCES[aa_type]["bounds"], RAMA_PREFERENCES[aa_type]["cmap"].N),
+				extent=(-180, 180, 180, -180))
+		ax.scatter(normals["phi"], normals["psi"],marker='o',s= 30,facecolors='blue', edgecolors= 'none', linewidth=1.0)
+		ax.scatter(outliers["phi"], outliers["psi"],marker='o',s= 30,facecolors='red', edgecolors= 'none', linewidth=1.0)
+		ax.set_xlabel('Phi')
+		ax.set_ylabel('Psi')
+		ax.set_title(res)
+		ax.set_xlim([-180,180])
+		ax.set_xticks([-180,-120,-60,0,60,120,180])
+		ax.set_yticks([-180,-120,-60,0,60,120,180])
+		ax.set_ylim([-180,180])
+		ax.grid(visible=True, which='major', axis='both',linestyle='--')
+		plt.tight_layout()
+		pdf.savefig()
+		plt.close()
+
+def plot_chi1_chi2_ramachandran(pdf, chi1DF, chi2DF):
+	print('Plotting Chi1-Chi2')
+	global ROTA_PREF_VALUES
+
+	if ROTA_PREF_VALUES is None:
+		ROTA_PREF_VALUES = _cache_ROTA_PREF_VALUES()
+
+	for res in chi1DF.index.to_list():
+		normals = {"chi1":[],"chi2":[]}
+		outliers = {"chi1":[],"chi2":[]}
+		aa_type = res[0]
+		multiple = float(ROTA_PREFERENCES[aa_type]["incr"])
+		for mnum in range(1,2,1):
+			# print(multiple * ceil(chi1DF.loc[res,mnum] / multiple))
+			# print(multiple * ceil(chi2DF.loc[res,mnum] / multiple))
+			# print(multiple * floor(chi1DF.loc[res,mnum] / multiple))
+			# print(multiple * floor(chi2DF.loc[res,mnum] / multiple))
+			if ROTA_PREF_VALUES[aa_type][int(multiple * floor(chi2DF.loc[res,mnum] / multiple))][int(multiple * floor(chi1DF.loc[res,mnum] / multiple))] < ROTA_PREFERENCES[aa_type]["bounds"][1]:
+				outliers["chi1"].append(chi1DF.loc[res,mnum])
+				outliers["chi2"].append(chi2DF.loc[res,mnum])
+			else:
+				normals["chi1"].append(chi1DF.loc[res,mnum])
+				normals["chi2"].append(chi2DF.loc[res,mnum])
+
+		fig, ax =plt.subplots(figsize=(3,3))
+		plt.imshow(ROTA_PREF_VALUES[aa_type], cmap=ROTA_PREFERENCES[aa_type]["cmap"],
+				norm=colors.BoundaryNorm(ROTA_PREFERENCES[aa_type]["bounds"], ROTA_PREFERENCES[aa_type]["cmap"].N),
+				extent=(0, 360, 360, 0))
+		ax.scatter(normals["chi1"], normals["chi2"],marker='o',s= 30,facecolors='blue', edgecolors= 'none', linewidth=1.0)
+		ax.scatter(outliers["chi1"], outliers["chi2"],marker='o',s= 30,facecolors='red', edgecolors= 'none', linewidth=1.0)
+		ax.set_xlabel('chi1')
+		ax.set_ylabel('chi2')
+		ax.set_title(res)
+		ax.set_xlim([0,360])
+		ax.set_xticks([0,60,120,180,240,300,360])
+		ax.set_yticks([0,60,120,180,240,300,360])
+		ax.set_ylim([0,360])
+		ax.grid(visible=True, which='major', axis='both',linestyle='--')
+		plt.tight_layout()
+		pdf.savefig()
+		plt.close()
+
+
+
+
 in_pdb = sys.argv[1]
 ###----------------------------------------------------------------------------###
 ##	Extract the starting and ending lines for the 20 NMR models in the file.	##
@@ -121,8 +249,8 @@ def extract(in_pdb, Sequence, outdir):
 			if line[0:4] == "ATOM" or line[0:4] == 'HETA':
 				index = '{:}{:}-{:}'.format(AAA_dict[line[17:20].strip()],line[22:26].strip(),line[12:16].strip())
 				Coor[index] = [float(line[30:38]),float(line[38:46]),float(line[46:54])]
-	PhiDF = pd.DataFrame(columns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,'mean','stdv'])
-	PsiDF = pd.DataFrame(columns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,'mean','stdv'])
+	PhiDF =  pd.DataFrame(columns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,'mean','stdv','type'])
+	PsiDF =  pd.DataFrame(columns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,'mean','stdv'])
 	chi1DF = pd.DataFrame(columns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,'mean','stdv'])
 	chi2DF = pd.DataFrame(columns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,'mean','stdv'])
 	#chi3DF = pd.DataFrame(columns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,'mean','stdv'])
@@ -147,6 +275,7 @@ def extract(in_pdb, Sequence, outdir):
 						if ang < 0: ang = ang + 360.0
 						diheDF.loc[res,mnum] = np.round(ang,1)
 
+
 	PhiDF['mean'] = np.round(PhiDF.mean(axis=1),2)
 	PhiDF['stdv'] = np.round(PhiDF.std(axis=1),2)
 	PsiDF['mean'] = np.round(PsiDF.mean(axis=1),2)
@@ -156,6 +285,15 @@ def extract(in_pdb, Sequence, outdir):
 	chi2DF['mean'] = np.round(chi2DF.mean(axis=1),2)
 	chi2DF['stdv'] = np.round(chi2DF.std(axis=1),2)
 
+	for i in range(1,len(Sequence)-1,1):
+		if Sequence[i+1][0] == 'P': PhiDF.loc[Sequence[i],'type'] = "PRE-PRO"
+		if Sequence[i][0] == 'P':PhiDF.loc[Sequence[i],'type'] = "PRO"
+		if Sequence[i][0] == 'G':PhiDF.loc[Sequence[i],'type'] = "GLY"
+		else: PhiDF.loc[Sequence[i],'type'] = "General"
+	for res in Sequence: 
+		if res[0] in SideDihe.keys():
+			chi1DF.loc[res,'type'] = res[0]
+
 	PhiDF.to_csv(outdir + outname.replace('.pdb', '_Phi.csv'))
 	PsiDF.to_csv(outdir + outname.replace('.pdb', '_Psi.csv'))
 	chi1DF.to_csv(outdir + outname.replace('.pdb', '_Chi1.csv'))
@@ -164,35 +302,25 @@ def extract(in_pdb, Sequence, outdir):
 	#chi4DF.to_csv(outdir + in_pdb.replace('.pdb', '_chi4.csv'))
 
 	pdf = PdfPages(outdir + outname.replace('.pdb', '_Phi-Psi.pdf'))
-	for res in PhiDF.index.to_list():
-		fig, ax =plt.subplots(figsize=(3,3))
-		ax.scatter(PhiDF.loc[res][:-2],PsiDF.loc[res][:-2],marker='o',s= 30,facecolors='none', edgecolors= 'blue', linewidth=1.0)
-		ax.set_xlabel('Phi')
-		ax.set_ylabel('Psi')
-		ax.set_title(res)
-		ax.set_xlim([-180,180])
-		ax.set_xticks([-180,-120,-60,0,60,120,180], labels=[-180,-120,-60,0,60,120,180])
-		ax.set_yticks([-180,-120,-60,0,60,120,180], labels=[-180,-120,-60,0,60,120,180])
-		ax.set_ylim([-180,180])
-		ax.grid(visible=True, which='major', axis='both',linestyle='--')
-		plt.tight_layout()
-		pdf.savefig()
-		plt.close()
+	plot_phi_psi_ramachandran(pdf,PhiDF, PsiDF)
 	pdf.close()
 
 	pdf = PdfPages(outdir + outname.replace('.pdb', '_Chi1-Chi2.pdf'))
-	for res in chi1DF.index.to_list():
-		fig, ax = plt.subplots(figsize=(3,3))
-		ax.scatter(chi1DF.loc[res][:-2],chi2DF.loc[res][:-2],marker='o',s= 30,facecolors='none', edgecolors= 'blue', linewidth=1.0)
-		ax.set_xlabel('Chi1')
-		ax.set_ylabel('Chi2')
-		ax.set_title(res)
-		ax.set_xlim([0,360])
-		ax.set_xticks([0,60,120,180,240,300,360], labels=[0,60,120,180,240,300,360])
-		ax.set_yticks([0,60,120,180,240,300,360], labels=[0,60,120,180,240,300,360])
-		ax.set_ylim([0,360])
-		ax.grid(visible=True, which='major', axis='both',linestyle='--')
-		plt.tight_layout()
-		pdf.savefig()
-		plt.close()
+	plot_chi1_chi2_ramachandran(pdf,chi1DF, chi2DF)
 	pdf.close()
+
+	# for res in chi1DF.index.to_list():
+	# 	fig, ax = plt.subplots(figsize=(3,3))
+	# 	ax.scatter(chi1DF.loc[res][:-2],chi2DF.loc[res][:-2],marker='o',s= 30,facecolors='none', edgecolors= 'blue', linewidth=1.0)
+	# 	ax.set_xlabel('Chi1')
+	# 	ax.set_ylabel('Chi2')
+	# 	ax.set_title(res)
+	# 	ax.set_xlim([0,360])
+	# 	ax.set_xticks([0,60,120,180,240,300,360])
+	# 	ax.set_yticks([0,60,120,180,240,300,360])
+	# 	ax.set_ylim([0,360])
+	# 	ax.grid(visible=True, which='major', axis='both',linestyle='--')
+	# 	plt.tight_layout()
+	# 	pdf.savefig()
+	# 	plt.close()
+	# pdf.close()
