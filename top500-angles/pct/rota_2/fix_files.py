4b028fdf9f7e561bd6af0d5e41f283fc0d601359
claymc3 <90853597+claymc3@users.noreply.github.com>
2023-03-20 11:30:32 -0500

    stashing changes before merg

--- top500-angles/pct/rota_2/fix_files.py
+++ top500-angles/pct/rota_2/fix_files.py
@@ -14,20 +14,41 @@ files = ['rota500-arg.data',
 'rota500-trp.data']
 # for i in range(0,360,10):
 # 	print(i)
-for i in range(4):
-	print(i)
+# for i in np.arange(5):
+# 	print(i)
 
 # intervlas = [np.arrange(0,15,5),np.arrange(0,10,1)]
 # for ran in intervlas:
 
 
-for file in files:
-	print(file)
-	ndim = open(file).readlines()[1][-2]
-	print(ndim)
-	increments = []
-	for x in range(3,int(ndim)+3,1):
-		increments.append([0.0, float(open(file).readlines()[x].split()[-3]),float(open(file).readlines()[x].split()[-3])/int(open(file).readlines()[x].split()[-2])])
-	print(increments)
-
-	
+# for file in files:
+# 	print(file)
+# 	ndim = open(file).readlines()[1][-2]
+# 	print(ndim)
+# 	increments = []
+# 	for x in range(3,int(ndim)+3,1):
+# 		increments.append([0.0, float(open(file).readlines()[x].split()[-3]),float(open(file).readlines()[x].split()[-3])/int(open(file).readlines()[x].split()[-2])])
+# 	print(increments)
+# count = 5 
+# trans_dict = {}
+# for i in np.arange(0,360,5):
+# 	for x in np.arange(0,360,5):
+# 		count+=1
+# 		for ni in np.arange(i,i+5,1):
+# 			for nx in np.arange(x,x+5,1):
+# 				print(ni,nx, count)
+				
+# print(count)
+MQ = {}
+count = 6 
+trans_dict = {}
+for i in np.arange(0,360,8):
+	for x in np.arange(0,360,8):
+		for y in np.arange(0,360,8):
+			count+=1
+			for ni in np.arange(i,i+8,1):
+				for nx in np.arange(x,x+8,1):
+					# for ny in np.arange(x,x+8,1):
+					MQ[count] = [ni,nx]
+					print(ni,nx, count)
+# print(count)
