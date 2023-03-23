c0e1053a1f0deb7426b5af82cb3787d5eda016c8
claymc3 <90853597+claymc3@users.noreply.github.com>
2023-03-22 20:13:35 -0500

    now using top8000

--- rama_config.py
+++ rama_config.py
@@ -4,22 +4,22 @@ import numpy as np
 
 RAMA_PREFERENCES = {
     "General": {
-        "file": os.path.join('top500-angles/pct/rama/', 'rama500-general.data'),
+        "file": os.path.join('top8000-angles/rama/', 'rama8000-general-noGPIVpreP.data'),
         "cmap": mplcolors.ListedColormap(['#FFFFFF', '#B3E8FF', '#7FD9FF']),
         "bounds": [0, 0.0005, 0.02, 1],
     },
     "GLY": {
-        "file": os.path.join('top500-angles/pct/rama/', 'rama500-gly-sym.data'),
+        "file": os.path.join('top8000-angles/rama/', 'rama8000-gly-sym.data'),
         "cmap": mplcolors.ListedColormap(['#FFFFFF', '#FFE8C5', '#FFCC7F']),
         "bounds": [0, 0.002, 0.02, 1],
     },
     "PRO": {
-        "file": os.path.join('top500-angles/pct/rama/', 'rama500-pro.data'),
+        "file": os.path.join('top8000-angles/rama/', 'rama8000-transpro.data'),
         "cmap": mplcolors.ListedColormap(['#FFFFFF', '#D0FFC5', '#7FFF8C']),
         "bounds": [0, 0.002, 0.02, 1],
     },
     "PRE-PRO": {
-        "file": os.path.join('top500-angles/pct/rama/', 'rama500-prepro.data'),
+        "file": os.path.join('top8000-angles/rama/', 'rama8000-prepro-noGP.data'),
         "cmap": mplcolors.ListedColormap(['#FFFFFF', '#E0D1FF', '#BE9EFF']),
         "bounds": [0, 0.002, 0.02, 1],
     }
@@ -27,69 +27,69 @@ RAMA_PREFERENCES = {
 
 ROTA_PREFERENCES = {
     "R": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-arg_chi1chi2.data'),
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-arg.data'),
         "cmap": mplcolors.ListedColormap(['#FFFFFF', '#D0FFC5', '#7FFF8C']),
-        "bounds": [0, 0.04, 0.04, 1],
+        "bounds": [0, 0.02, 0.2, 1],
     },
     "N": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-asn_chi1chi2.data'),
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-asn.data'),
         "cmap": mplcolors.ListedColormap(['#FFFFFF', '#D0FFC5', '#7FFF8C']),
-        "bounds": [0, 0.05, 0.1, 1],
+        "bounds": [0, 0.02, 0.2, 1],
     },
     "D": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-asp_chi1chi2.data'),
-        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#D0FFC5', '#7FFF8C']),
-        "bounds": [0, 0.05, 0.1, 1],
-    },
-    "I": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-ile_chi1chi2.data'),
-        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#B3E8FF', '#7FD9FF']),
-        "bounds": [0, 0.03, 0.1, 1],
-    },
-    "L": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-leu_chi1chi2.data'),
-        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#B3E8FF', '#7FD9FF']),
-        "bounds": [0, 0.05, 0.1, 1],
-    },
-    "M": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-met_chi1chi2.data'),
-        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#B3E8FF', '#7FD9FF']),
-        "bounds": [0, 0.2, 0.3, 1],
-    },
-    "F": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-phetyr_chi1chi2.data'),
-        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#E0D1FF', '#BE9EFF']),
-        "bounds": [0, 0.05, 0.1, 1],
-    },
-    "Y": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-phetyr_chi1chi2.data'),
-        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#E0D1FF', '#BE9EFF']),
-        "bounds": [0, 0.05, 0.1, 1],
-    },
-    "W": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-trp_chi1chi2.data'),
-        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#E0D1FF', '#BE9EFF']),
-        "bounds": [0, 0.05, 0.1, 1],
-    },
-    "H": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-his_chi1chi2.data'),
-        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#E0D1FF', '#BE9EFF']),
-        "bounds": [0, 0.05, 0.1, 1],
-    },
-    "E": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-glu_chi1chi2.data'),
-        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#D0FFC5', '#7FFF8C']),
-        "bounds": [0, 0.05, 0.1, 1],
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-asp.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#FFE8C5', '#FFCC7F']),
+        "bounds": [0, 0.02, 0.2, 1],
     },
     "Q": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-gln_chi1chi2.data'),
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-gln.data'),
         "cmap": mplcolors.ListedColormap(['#FFFFFF', '#D0FFC5', '#7FFF8C']),
-        "bounds": [0, 0.05, 0.1, 1],
+        "bounds": [0, 0.02, 0.2, 1],
+    },
+    "E": {
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-glu.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#FFE8C5', '#FFCC7F']),
+        "bounds": [0, 0.02, 0.2, 1],
+    },
+    "H": {
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-his.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#E0D1FF', '#BE9EFF']),
+        "bounds": [0, 0.02, 0.2, 1],
+    },
+    "I": {
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-ile.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#B3E8FF', '#7FD9FF']),
+        "bounds": [0, 0.02, 0.2, 1],
+    },
+    "L": {
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-leu.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#B3E8FF', '#7FD9FF']),
+        "bounds": [0, 0.02, 0.2, 1],
     },
     "K": {
-        "file": os.path.join('top8000-angles/rota/', 'rota8000-lys_chi1chi2.data'),
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-lys.data'),
         "cmap": mplcolors.ListedColormap(['#FFFFFF', '#D0FFC5', '#7FFF8C']),
-        "bounds": [0, 0.05, 0.1, 1],
+        "bounds": [0, 0.02, 0.2, 1],
+    },
+    "M": {
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-met.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#B3E8FF', '#7FD9FF']),
+        "bounds": [0, 0.04, 0.2, 1],
+    },
+    "F": {
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-phetyr.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#E0D1FF', '#BE9EFF']),
+        "bounds": [0, 0.05, 0.2, 1],
+    },
+    "Y": {
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-phetyr.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#E0D1FF', '#BE9EFF']),
+        "bounds": [0, 0.05, 0.2, 1],
+    },
+    "W": {
+        "file": os.path.join('top8000-angles/rota/', 'rota8000-trp.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#E0D1FF', '#BE9EFF']),
+        "bounds": [0, 0.02, 0.2, 1],
     }
 }
 
