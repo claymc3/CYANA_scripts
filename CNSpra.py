4d6f1a3153fcd213215d05d9948481c27fdad611
Mary Clay <mary.clay@stjude.org>
2024-06-14 17:57:17 -0500

    handeling phi/psi < -180 and > 180 better, still need to work on the violation info

--- CNSpra.py
+++ CNSpra.py
@@ -331,6 +331,7 @@ for x in range(len(dihetbl)):
         othercount+=1
       if 'CHI' in angle and mang < 0: mang = mang + 360
       ang_min = np.round(float(mang) - float(line2.split()[14]),3)
+      ang_min_orig = np.round(float(mang) - float(line2.split()[14]),3)
       ang_max = np.round(float(mang) + float(line2.split()[14]),3)
       if 'P' in angle: 
         angdict = phipsidict
@@ -349,25 +350,29 @@ for x in range(len(dihetbl)):
         angdict[Seqdict[resn]] = [[outline,'black']]
       else: 
         angdict[Seqdict[resn]].append([outline,'black'])
-      if ang_min <= -180.0:ang_min = ang_min + 360.0
+      if ang_min <= -180.0:
+        ang_min = ang_min + 360.0
+        if ang_max < 0:
+          ang_max = ang_max + 360.0
       # if ang_max > 180:
 
       angles = []
       for mnum in range(1,21,1):
         Coords = eval('Coor' + str(mnum))
         ang = np.round(Dihed.calcDihedrals(Coords[a1],Coords[a2],Coords[a3],Coords[a4]),1)
-        if ang_min > 180.0: ang = ang + 360.0
+        if 'P' in angle and ang_min_orig < -180.0: ang = ang + 360.0
+        if 'P' in angle and ang_max > 180.0 and ang < 0.0: ang = ang + 360.0
         # if ang < 0: ang = ang + 360.0
-        if 'CHI' in angle and ang < 0: ang = ang + 360.0
+        if 'CHI' in angle and ang < 0.0: ang = ang + 360.0
         # if 'P' in angle and ang_max > 180:  ang_min = ang_max - 360.0 
         if np.round(ang_min - ang,1) > 5.0 :
-          print('{:} max {:} min {:}'.format(num2AAA[resn]+angle,ang_max,ang_min))
+          print('{:} max {:} min {:}'.format(num2AAA[resn]+resn+angle,ang_max,ang_min))
           print('min {:} val {:} min {:} diff {:}'.format(num2AAA[resn]+angle, ang, ang_min,np.round(ang_min - ang,1)))
           angles.append(np.round(ang_min - ang,1))
           AngleViolDF.loc[Seqdict[resn]+angle,mnum] = np.round(ang_min - ang,1)
         if np.round(ang - ang_max,1) > 5.0 : 
           angles.append(np.round(ang - ang_max,1))
-          print('{:} max {:} min {:}'.format(num2AAA[resn]+angle,ang_max,ang_min))
+          print('{:} max {:} min {:}'.format(num2AAA[resn]+resn+angle,ang_max,ang_min))
           print('max {:} val {:} max {:} diff {:}'.format(num2AAA[resn]+angle, ang, ang_max,np.round(ang - ang_max,1)))
           AngleViolDF.loc[Seqdict[resn]+angle,mnum] = np.round(ang - ang_max,1)
       if len(angles) > 5:
