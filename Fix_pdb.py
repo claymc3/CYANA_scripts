23bb925afdcbe24cd9dbb6c9ee2fd09f355fb969
Mary Clay <mary.clay@stjude.org>
2025-03-13 11:13:54 -0500

    updated tp cprrect for phosphotyrosine, classifying as HETATM and fixing atom names.

--- Fix_pdb.py
+++ Fix_pdb.py
@@ -12,29 +12,55 @@ if len(sys.argv)==1:
   print('''
 
 Usage: 
-    fixpdb [name] [numbering correction]
+    fixpdb [PDB] [numbering correction]
 
 Required Input:
 
-    name        assuming your running script in CNS/refinePDB/ directory 
+    PDB         assuming your running script in CNS/refinePDB/ directory 
                 name is the name you supplied to the cnsprep and cnspost
                 scripts 
 
     numbering correction    how much should be added to correct the numbering
 
 OutPut:
-    name_noe_rn.tbl
-    name_dihed_rn.tbl
-    name_hbond_rn.tbl
+    PDB_fix.tbl
+
 ''')
 
 newpdb = open(inpdb.replace('.pdb','_fixed.pdb'),'w')
+# for line in open(inpdb).readlines():
+#   if re.match(r"^ATOM", line):
+#       resid = '{:<2.0f}'.format(float(line[22:26].strip()) + correction)
+#       newline = "{:} {:} {:<4}{:}".format(line[0:20],line[72],resid,line[27:])
+#       newpdb.write(newline)
+#   if not re.match(r"^ATOM", line):
+#       newpdb.write(line)
+# newpdb.close()
 for line in open(inpdb).readlines():
-	if re.match(r"^ATOM", line):
-		resid = '{:<2.0f}'.format(float(line[22:26].strip()) + correction)
-
-		newline = "{:} {:} {:<4}{:}".format(line[0:20],line[72],resid,line[27:])
-		newpdb.write(newline)
-	if not re.match(r"^ATOM", line):
-		newpdb.write(line)
+  if re.match(r"PTR", line[17:20].strip()):
+    resid = '{:<2.0f}'.format(float(line[22:26].strip()) + correction)
+    line1 = line.replace('ATOM  ','HETATM').replace('1HE ',' HE1').replace('2HE ',' HE2').replace('1HD ',' HD1').replace('2HD ',' HD2')
+    resid = '{:<2.0f}'.format(float(line[22:26].strip()) + correction)
+    newline = "{:} {:} {:<4}{:}".format(line1[0:20],line[72],resid,line1[27:])
+    # newline = "{:} {:}{:}".format(line1[0:20],line[72],line1[22:])
+    newpdb.write(newline)
+  if re.match(r"^ATOM", line) and not re.match(r"PTR", line[17:20].strip()):
+    newline = "{:} {:}{:}".format(line[0:20],line[72],line[22:])
+    newpdb.write(newline)
+  if not re.match(r"^ATOM", line):
+    newpdb.write(line)
 newpdb.close()
+
+  # if re.match(r"TPO", line[17:20].strip()):
+  #   print(line)
+  #   resid = '{:<2.0f}'.format(float(line[22:26].strip()) + correction)
+  #   line1 = line.replace('ATOM  ','HETATM').replace('1HG2','HG21').replace('2HG2','HG22').replace('3HG2','HG23')
+  #   newline = "{:} {:} {:<4}{:}".format(line1[0:20],line[72],resid,line1[27:])
+  #   newpdb.write(newline)
+
+  # if re.match(r"SEP", line[17:20].strip()):
+  #   print(line)
+  #   resid = '{:<2.0f}'.format(float(line[22:26].strip()) + correction)
+  #   line1 = line.replace('ATOM  ','HETATM')
+  #   newline = "{:} {:} {:<4}{:}".format(line1[0:20],line[72],resid,line1[27:])
+  #   newpdb.write(newline)
